ded4f4a052ceed516468fc8f0d358047c2b801f7
tlc2880 <tommylcao@gmail.com>
2023-02-14 05:00:02 -0800

    complete-register

--- src/controllers/auth.js
+++ src/controllers/auth.js
@@ -1,18 +1,32 @@
 const db = require('../db')
+const { hash } = require('bcryptjs')
 
 exports.getUsers = async (req, res) => {
     try {
-        const { rows } = await db.query('select * from users')
-        console.log(rows)
+        const { rows } = await db.query('SELECT user_id, email FROM users')
+        return res.status(200).json({
+            success: true,
+            users: rows
+        })
     } catch (error) {
         console.log(error.message)
     }
 }
 
 exports.register = async (req, res) => {
+    const { email, password } = req.body
     try {
-        console.log('validation passed')
+        const hashedPassword = await hash(password, 10);
+        await db.query('INSERT INTO users(email, password) VALUES ($1, $2)', [email, hashedPassword]);
+
+        return res.status(201).json({
+            success: true,
+            message: 'The registration was successful.'
+        })
     } catch (error) {
         console.log(error.message)
+        return res.status(500).json({
+            error: error.message
+        })
     }
 }
