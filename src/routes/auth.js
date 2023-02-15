fd65959ce59af3bf79eee6d29a41cd7cd2b88542
tlc2880 <tommylcao@gmail.com>
2023-02-15 05:54:33 -0800

    Validate login

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -1,10 +1,17 @@
 const { Router } = require('express')
-const { getUsers, register } = require('../controllers/auth')
-const { validationMiddleware } = require('../middlewares/validations-middleware')
-const { registerValidation } = require('../validators/auth')
+const {
+  getUsers,
+  register,
+  login
+} = require('../controllers/auth')
+const {
+  validationMiddleware,
+} = require('../middlewares/validations-middleware')
+const { registerValidation, loginValidation } = require('../validators/auth')
 const router = Router()
 
 router.get('/get-users', getUsers)
-router.post('/register',  registerValidation, validationMiddleware, register)
+router.post('/register', registerValidation, validationMiddleware, register)
+router.post('/login', loginValidation, validationMiddleware, login)
 
 module.exports = router
--- src/validators/auth.js
+++ src/validators/auth.js
@@ -1,5 +1,6 @@
 const { check } = require('express-validator')
 const db = require('../db')
+const { compare } = require('bcryptjs')
 
 // password
 const password = check('password').isLength({ min: 6, max: 15 }).withMessage('Password has to be between 6 to 15 characters.')
@@ -17,6 +18,24 @@ const emailExists = check('email').custom(async (value) => {
     }
 })
 
+//login validation
+const loginFieldsCheck = check('email').custom(async (value, { req }) => {
+    const user = await db.query('SELECT * from users WHERE email = $1', [value])
+  
+    if (!user.rows.length) {
+      throw new Error('Email does not exists.')
+    }
+  
+    const validPassword = await compare(req.body.password, user.rows[0].password)
+  
+    if (!validPassword) {
+      throw new Error('Wrong password')
+    }
+  
+    req.user = user.rows[0]
+  })
+
 module.exports = {
-    registerValidation: [email, password, emailExists], 
+    registerValidation: [email, password, emailExists],  
+    loginValidation: [loginFieldsCheck],
 }
