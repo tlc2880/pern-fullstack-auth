77b7018c3570fb8c52eba9346408ea2538ea0ae3
tlc2880 <tommylcao@gmail.com>
2023-02-17 05:34:31 -0800

    Add the logout feature i controllers and routes

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -4,6 +4,7 @@ const {
   register,
   login,
   protected,
+  logout,
 } = require('../controllers/auth')
 const {
   validationMiddleware,
@@ -16,5 +17,6 @@ router.get('/get-users', getUsers)
 router.get('/protected', userAuth, protected)
 router.post('/register', registerValidation, validationMiddleware, register)
 router.post('/login', loginValidation, validationMiddleware, login)
+router.get('/logout', logout)
 
 module.exports = router
