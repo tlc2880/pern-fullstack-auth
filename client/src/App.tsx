002700560815812af70a056c4d91f6eb3825e6cc
tlc2880 <tommylcao@gmail.com>
2023-02-18 06:36:00 -0800

    Initial commit for the client

---
+++ client/src/App.tsx
@@ -0,0 +1,42 @@
+import React from 'react';
+import {
+  BrowserRouter,
+  Navigate,
+  Routes,
+  Route,
+  Outlet,
+} from 'react-router-dom'
+
+import Dashboard from './pages/Dashboard'
+import Home from './pages/Home'
+import Login from './pages/Login'
+import Register from './pages/Register'
+
+const PrivateRoutes = () => {
+  const isAuth = false;
+  return <>{isAuth ? <Outlet /> : <Navigate to='/login' />}</>
+}
+
+const RestrictedRoutes = () => {
+  const isAuth = false;
+  return <>{!isAuth ? <Outlet /> : <Navigate to='/dashboard' />}</>
+}
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route element={<PrivateRoutes />}>
+          <Route path='/dashboard' element={<Dashboard />} />
+        </Route>
+        <Route element={<RestrictedRoutes />}>
+          <Route path='/register' element={<Register />} />
+          <Route path='/login' element={<Login />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
