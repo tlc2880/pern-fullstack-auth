96a3721a53f5022f52686b2966724d6cec06659c
tlc2880 <tommylcao@gmail.com>
2023-02-18 06:56:49 -0800

    Correct initial client error

--- client/src/pages/Dashboard.tsx
+++ client/src/pages/Dashboard.tsx
@@ -1,11 +1,8 @@
 import React from 'react';
-import Layout from '../components/Layout';
 
 function Dashboard() {
   return (
-    <Layout>
-      <h1>Dashboard</h1>
-    </Layout>
+    <h1>Dashboard</h1>
   );
 }
 
