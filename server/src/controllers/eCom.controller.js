13e29858183e224a5cda1b3b2fa6c6170991f22f
tlc2880 <tommylcao@gmail.com>
2023-03-02 04:58:17 -0800

    strip-env

---
+++ server/src/controllers/eCom.controller.js
@@ -0,0 +1,31 @@
+// create todo
+const stripe = require('stripe')('sk_test_LTQPhkCZjMU3La6mWPYzmJ6L00qXdcCpaO');
+const checkout = async (req, res) => {
+    console.log(req.body);
+    const items = req.body.items;
+    let lineItems = [];
+    items.forEach((item)=> {
+        lineItems.push(
+            {
+                price: item.id,
+                quantity: item.quantity
+            }
+        )
+    });
+
+    const session = await stripe.checkout.sessions.create({
+        line_items: lineItems,
+        mode: 'payment',
+        success_url: "http://localhost:3000/success",
+        cancel_url: "http://localhost:3000/cancel"
+    });
+
+    res.send(JSON.stringify({
+        url: session.url
+    }));
+};
+
+
+module.exports = {
+    checkout
+}
