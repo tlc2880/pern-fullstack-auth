3e4b6897524bc8246360fe8779f21396d3cfb2c6
tlc2880 <tommylcao@gmail.com>
2023-03-08 04:13:25 -0800

    Add server eCommerce get and create one item

--- server/src/controllers/shop.controller.js
+++ server/src/controllers/shop.controller.js
@@ -10,6 +10,33 @@ const getShopItems = async (req, res) => {
     }
 }
 
+// get one item
+const getShopItem = async (req, res) => {
+    try {
+        const { id_shop } = req.params;
+        const item = await pool.query("SELECT * FROM shop WHERE id_shop = $1", [id_shop])
+        res.json(item.rows[0])
+    } catch (error) {
+        console.error(error.message)
+    }
+}
+
+// create one item
+const createItem = async (req, res) => {
+    try {
+        const {
+            title, price, id
+        } = req.body
+        const newItem = await pool.query("INSERT INTO shop (title, price, id) VALUES($1, $2, $3) RETURNING *", 
+            [title, price, id])
+        res.json(newItem.rows[0])
+    } catch (error) {
+        console.error(error.message)
+    }
+}
+
 module.exports = {
-    getShopItems
+    getShopItems,
+    getShopItem,
+    createItem
 }
