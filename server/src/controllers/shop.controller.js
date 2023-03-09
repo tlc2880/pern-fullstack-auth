530673d30fa3b98cb3221c98286e54c3027ad8ef
tlc2880 <tommylcao@gmail.com>
2023-03-09 05:17:29 -0800

    Add server  update and delete one item

--- server/src/controllers/shop.controller.js
+++ server/src/controllers/shop.controller.js
@@ -1,4 +1,30 @@
 const pool = require("../db");
+const stripe = require('stripe')('sk_test_LTQPhkCZjMU3La6mWPYzmJ6L00qXdcCpaO');
+
+const checkout = async (req, res) => {
+    console.log(req.body);
+    const items = req.body.items;
+    let lineItems = [];
+    items.forEach((item)=> {
+        lineItems.push(
+            {
+                price: item.id,
+                quantity: item.quantity
+            }
+        )
+    });
+
+    const session = await stripe.checkout.sessions.create({
+        line_items: lineItems,
+        mode: 'payment',
+        success_url: "http://localhost:3000/success",
+        cancel_url: "http://localhost:3000/cancel"
+    });
+
+    res.send(JSON.stringify({
+        url: session.url
+    }));
+};
 
 // get all shop items
 const getShopItems = async (req, res) => {
@@ -21,7 +47,7 @@ const getShopItem = async (req, res) => {
     }
 }
 
-// create one item
+// create an item
 const createItem = async (req, res) => {
     try {
         const {
@@ -35,8 +61,37 @@ const createItem = async (req, res) => {
     }
 }
 
+  // update one item
+  const updateItem = async (req, res) => {
+    try {
+        const { id_shop } = req.params;
+        const {
+            title, price, id
+        } = req.body;
+        const editItem = await pool.query("UPDATE shop SET title=$1, price=$2, id=$3 WHERE id_shop = $4", 
+            [title, price, id, id_shop])
+            res.json("Item was updated")
+    } catch (error) {
+        console.error(error.message)
+    }
+};
+
+  // delete an item
+  const deleteItem = async (req, res) => {
+    try {
+        const { id_shop } = req.params;
+        const deleteItem = await pool.query("DELETE FROM shop WHERE id_shop = $1", [id_shop])
+        res.json("Item was deleted!")
+    } catch (error) {
+        console.error(error.message)
+    }
+};
+
 module.exports = {
+    checkout,
     getShopItems,
     getShopItem,
-    createItem
+    createItem,
+    updateItem,
+    deleteItem
 }
--- server/src/routes/shop.route.js
+++ server/src/routes/shop.route.js
@@ -1,13 +1,19 @@
 const router = require("express").Router();
 
 const {
+    checkout,
     getShopItems,
     getShopItem,
-    createItem
+    createItem,
+    updateItem,
+    deleteItem
 } = require("../controllers/shop.controller");
 
+router.post('/shop/checkout', checkout);
 router.get("/shop", getShopItems);
 router.get('/shop/:id_shop', getShopItem);
 router.post('/shop', createItem);
+router.put('/shop/:id_shop', updateItem);
+router.delete('/shop/:id_shop', deleteItem);
  
 module.exports = router;
