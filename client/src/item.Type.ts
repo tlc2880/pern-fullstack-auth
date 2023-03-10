6bfb2e9f25e92c59466404ceb5386873079e1631
tlc2880 <tommylcao@gmail.com>
2023-03-10 05:09:52 -0800

    Implement client eCommerce shop getItems

---
+++ client/src/item.Type.ts
@@ -0,0 +1,6 @@
+export default interface ItemType {
+    id: string,
+    title: string,
+    price: number,
+    id_shop: string
+}
--- client/src/redux/store.ts
+++ client/src/redux/store.ts
@@ -1,11 +1,13 @@
 import { configureStore } from '@reduxjs/toolkit'
 import authSlice from './slices/authSlice'
 import todoSlice from './slices/todoSlice'
+import shopSlice from './slices/shopSlice'
 export type AppDispatch = typeof store.dispatch;
 export type RootState = ReturnType<typeof store.getState>;
 export const store = configureStore({
     reducer: {
         auth: authSlice,
-        todo: todoSlice
+        todo: todoSlice,
+        shop: shopSlice
     }
 })
