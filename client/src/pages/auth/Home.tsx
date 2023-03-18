8d3d52ce16142b16051b6078075fb3bf05b85e4a
tlc2880 <tommylcao@gmail.com>
2023-03-18 08:39:12 -0700

    Add shop item update to all card items

--- client/src/pages/auth/Home.tsx
+++ client/src/pages/auth/Home.tsx
@@ -1,21 +1,31 @@
 import Layout from '../../components/Layout';
-import { ItemUpdate } from '../shop/ItemUpdate';
-const itemInit = {
-  id: "price_5555555",
-  title: "Blue Pens",
-  price: 1.39,
-  id_shop: 21
-}
+import {Row, Col, Container} from 'react-bootstrap';
+import useProducts from "../shop/useProducts";
+import ItemCard from '../shop/ItemCard';
+import CartProvider from '../shop/CartContext';
+import itemType from '../../item.Type'
 
 function Home() {
+  const { productsArray } = useProducts();
+
   return (
-    <Layout>
-      <>
-        <h1>Home page</h1>
-        < ItemUpdate item={itemInit} />
-      </>
-    </Layout>
-  );
+    <>
+      <Layout>
+        <CartProvider>
+          <Container>
+            <h1 className="p-3">Administration of Store Items</h1>
+            <Row xs={1} md={3} className="g-4">
+              {productsArray && productsArray.map((item: itemType) => (
+                <Col align="center" key={item.id_shop}>
+                  <ItemCard product={item} />
+                </Col>
+              ))}
+            </Row>
+          </Container>
+        </CartProvider>
+      </Layout>
+    </>
+  )
 }
 
 export default Home;
