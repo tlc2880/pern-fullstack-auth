f46237c3abfb77bb1a393067f11f11530450d72b
tlc2880 <tommylcao@gmail.com>
2023-03-19 05:44:23 -0700

    Add client delete item card

--- client/src/pages/shop/ItemCard.tsx
+++ client/src/pages/shop/ItemCard.tsx
@@ -1,3 +1,5 @@
+import { useAppSelector, useAppDispatch } from "../../redux/hooks";
+import { deleteItem } from "../../redux/slices/shopSlice";
 import { CartContext } from './CartContext';
 import { useContext } from 'react';
 import { ItemUpdate } from './ItemUpdate';
@@ -5,6 +7,8 @@ import Card from "@mui/material/Card";
 import CardActions from "@mui/material/CardActions";
 import CardContent from "@mui/material/CardContent";
 import Typography from "@mui/material/Typography";
+import IconButton from "@mui/material/IconButton";
+import DeleteIcon from "@mui/icons-material/Delete";
 import useProducts from "./useProducts";
 import itemType from '../../item.Type'
 
@@ -13,33 +17,48 @@ type ItemCardProps = {
   }
 
 const ItemCard = ( props: ItemCardProps ) => {
+    const shop = useAppSelector((state) => state.shop);
+    const dispatch = useAppDispatch();
+
     const { productsArray } = useProducts();
     const product = props.product;
     const cart = useContext(CartContext);
     console.log(cart.items);
     console.log(productsArray)
+
+    const handleDelete = (id: number) => {
+        dispatch(deleteItem(id));
+    };
+
     return (
-        <>
-            <Card sx={{ minWidth: 300 }}>
-            <CardContent>
-                <Typography align="left" sx={{ fontSize: 18 }} gutterBottom>
-                    Title: {product.title}
-                </Typography>
-                <Typography align="left" sx={{ fontSize: 18 }} gutterBottom>
-                    Price: ${product.price}
-                </Typography>
-                <Typography align="left" sx={{ fontSize: 18 }} gutterBottom>
-                    ID: {product.id}
-                </Typography>
-                <Typography align="left" sx={{ fontSize: 18 }} gutterBottom>
-                    Id_shop: {product.id_shop}
-                </Typography>
-            </CardContent>
-            <CardActions>
-                < ItemUpdate item={product}/>
-            </CardActions>
-            </Card>
-        </>
+    <>
+        <Card sx={{ minWidth: 300 }}>
+        <CardContent>
+            <Typography align="left" sx={{ fontSize: 18 }} gutterBottom>
+                Title: {product.title}
+            </Typography>
+            <Typography align="left" sx={{ fontSize: 18 }} gutterBottom>
+                Price: ${product.price}
+            </Typography>
+            <Typography align="left" sx={{ fontSize: 18 }} gutterBottom>
+                ID: {product.id}
+            </Typography>
+            <Typography align="left" sx={{ fontSize: 18 }} gutterBottom>
+                Id_shop: {product.id_shop}
+            </Typography>
+        </CardContent>
+        <CardActions>
+            < ItemUpdate item={product}/>
+            <IconButton
+                component="button"
+                onClick={() => handleDelete(product.id_shop)}
+                color="error"
+            >
+                <DeleteIcon />
+            </IconButton>
+        </CardActions>
+        </Card>
+    </>
     )
 }
 
